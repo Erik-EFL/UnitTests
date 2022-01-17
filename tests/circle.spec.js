c6067f15189218cd50cd7951ecdba2eaec2a517f
Erik-EFL <erik.lima.f@outlook.com>
2022-01-17 20:25:25 -0300

    Adicionando requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const circle = require('../src/circle');
+const circle = require("../src/circle");
 
 /*
   Essa função recebe o raio de um círculo e retorna um objeto contendo suas informações (Raio, Área e Circunferência).
@@ -23,16 +23,15 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
+describe("4 - Implemente os casos de teste para a função `circle`", () => {
+  it("Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados", () => {
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    expect(circle('')).toBeUndefined();
+    expect(circle(1)).toEqual({area: 3.14, circumference: 6.28, radius: 1});
+    expect(Object.keys(circle(1)).length).toEqual(3);
+    expect(circle()).toBeUndefined();
+    expect(circle(2).circumference).toEqual(12.56);
+    expect(circle(3).area).toBe(28.259999999999998);
+    expect(circle(3)).toEqual({area: 28.259999999999998, circumference: 18.84, radius: 3});
   });
 });
