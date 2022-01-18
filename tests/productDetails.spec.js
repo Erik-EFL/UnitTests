aba207c7806bf4553d190e3c3281ee6da25f32a9
Erik-EFL <erik.lima.f@outlook.com>
2022-01-18 13:40:19 -0300

    adicionando requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,4 +1,4 @@
-const productDetails = require('../src/productDetails');
+const productDetails = require("../src/productDetails");
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -8,12 +8,12 @@ const productDetails = require('../src/productDetails');
   - Uma string;
 
   Comportamento:
-  productDetails('Alcool gel', 'Máscara') // Retorna:
+  productDetails('Álcool gel', 'Máscara') // Retorna:
   [
     {
-      name: 'Alcool gel'
+      name: 'Álcool gel'
       details: {
-        productId: 'Alcool gel123'
+        productId: 'Álcool gel123'
       }
     },
     {
@@ -26,15 +26,32 @@ const productDetails = require('../src/productDetails');
 
 */
 
-describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+describe("6 - Implemente os casos de teste para a função `productDetails`", () => {
+  it("Verifica se a função `productDetails` tem o comportamento esperado", () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe("function");
+
     // Teste se o retorno da função é um array.
+    const isObject = typeof productDetails("Luva", "Mascara"); // Esta linha de código foi sugerida por Lais Namatela
+    expect(Array.isArray(productDetails("Luva", "Mascara"))).toBeTruthy();
+
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(Object.keys(productDetails("Luva", "Mascara")).length).toBe(2);
+
     // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    const firstObject = typeof productDetails("Luva", "Mascara")[0];
+    const secondObject = typeof productDetails("Luva", "Mascara")[1];
+    expect(firstObject && secondObject).toBe("object");
+
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são
+    // diferentes entre si.
+    expect(isObject[0]).not.toEqual(isObject[1]);
     // Teste se os dois productIds terminam com 123.
+
+    const keyOne = productDetails("Luva", "Mascara")[0];
+    const keyTwo = productDetails("Luva", "Mascara")[1];
+    expect(keyOne.details.productId).toMatch(/(.*123$)/);
+    expect(keyTwo.details.productId).toMatch(/(.*123$)/);
   });
 });
